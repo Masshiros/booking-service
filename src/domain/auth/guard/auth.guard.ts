9cdd76c770a83b817c98e4a99e34699f074f279d
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-09 11:28:14 +0700

    Feat(Auth): Implement public decorator

--- src/domain/auth/guard/auth.guard.ts
+++ src/domain/auth/guard/auth.guard.ts
@@ -4,19 +4,29 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 
 import { Request } from 'express';
 import { DatabaseService } from 'src/database/database.service';
+import { IS_PUBLIC_KEY } from 'src/share/decorator/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private databaseService: DatabaseService,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
