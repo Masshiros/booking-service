a235fe746d1e1d278b729cd468462e9fec99a687
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-09 09:53:28 +0700

    Feat(user): add hash password*

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(private databaseService: DatabaseService) {}
   findMany() {
     return this.databaseService.user.findMany();
   }
-  register(data: Prisma.UserCreateInput) {
-    return this.databaseService.user.create({ data });
+  async register(data: Prisma.UserCreateInput) {
+    const userData = {
+      email: data.email,
+      password: await this.hashPassword(data.password),
+      firstName: data.firstName,
+      lastName: data.lastName,
+      phone: data.phone,
+      timezoneCode: data.timezoneCode,
+    };
+    return this.databaseService.user.create({ data: userData });
+  }
+  async hashPassword(password: string) {
+    const salt = await bcrypt.genSaltSync(10);
+    return await bcrypt.hash(password, salt);
   }
 }
